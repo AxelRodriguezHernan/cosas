15c34958283d7f72a6acf9751b9905dd4eab2e0f
AxelRodriguezHernan <141934324+AxelRodriguezHernan@users.noreply.github.com>
2023-10-17 09:47:30 -0300

    Add files via upload

---
+++ arbolgrafoclase/src/main.java
@@ -0,0 +1,54 @@
+public class main {
+
+    Node elto = new Node(valor)
+
+            if(THIS.PRIMERnODO == null)
+    {
+        primerNodo = elto
+        return;
+    }
+
+    elto.next ? primerNodo;
+    rpimerNodo = elto;
+
+}
+
+public void agregarFinal(int valor){
+
+    if(this.primerNodo == null){
+        primerNodo = elto;
+        retur;
+
+
+
+
+
+        public boolean incluyeValor(Int valor){
+
+            Node aux = primerNodo;
+
+            while( aux.data != valor){
+
+                if(aux.next == null){
+                    return false
+                }
+                 aux = aux.next;
+            }
+
+            return true;
+    }
+
+    public void eliminarValor(int valor){
+
+
+
+
+    }
+
+    public void eliminarPosicion(int posicion){
+
+
+
+    }
+    }
+}
